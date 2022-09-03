fe32c6314aea5e6f44d07289197dab20ee4091b0
Amir <asp20@st-andrews.ac.uk>
2022-09-03 19:02:43 +0100

    front-end component for playlist creation feature

---
+++ frontend/src/components/PlaylistCanvas.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PlaylistCanvas = (props) => {
+    const {   
+        primaryColor,
+        secondaryColor
+    } = props
+
+    // const colorChange = () => {
+    //
+    // }
+
+    return (
+        <canvas>Actual Painting</canvas>
+    )
+}
+
+export default PlaylistCanvas;
