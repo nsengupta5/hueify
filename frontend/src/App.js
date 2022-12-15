4d89f185a35c5364c555627b7d1085031e57ea62
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2022-12-15 13:18:48 +0000

    Colors updated with background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './assets/text.css';
 import Layout from "./components/Layout";
 import AlbumSearch from "./components/AlbumSearch";
 import { Link } from "react-router-dom";
 
 const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState("#43baae");
+
+  const getBackgroundImage = (data) => {
+    console.log(data);
+    setBackgroundColor(data);
+  }
+
   return (
-    <Layout>
-        <AlbumSearch/>
-    </Layout>
+    <div className="h-full min-h-screen" style={{backgroundImage: `linear-gradient(to bottom, ${backgroundColor}, white)`}}>
+      <Layout>
+          <AlbumSearch func={getBackgroundImage}/>
+      </Layout>
+    </div>
   );
 }
 
