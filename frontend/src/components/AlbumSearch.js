898171135e9d49bccb465d047587b65d7a1d7e28
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2022-12-30 13:04:14 +0800

    Processing indicator added

--- frontend/src/components/AlbumSearch.js
+++ frontend/src/components/AlbumSearch.js
@@ -53,44 +53,53 @@ const AlbumSearch = (props) => {
 
     return (
         <>
-        <div className="flex justify-center mt-8">
-            <div className="mb-3 xl:w-96">
-                <div className="relative flex items-stretch w-full mb-4 input-group">
-                    <input type="search" className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Input song or album link" aria-label="Search" aria-describedby="button-addon3" value={url} onChange={e => setUrl(e.target.value)} />
-                    <button className="inline-block px-6 py-2 text-xs font-medium leading-tight uppercase bg-gray-900 border-2 border-gray-900 rounded btn text-gray-50 hover:bg-black focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3" onClick={getAlbum}>GO</button>
+            <div className="flex justify-center mt-8">
+                <div className="mb-3 xl:w-96">
+                    <div className="relative flex items-stretch w-full mb-4 input-group">
+                        <input type="search" className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Input song or album link" aria-label="Search" aria-describedby="button-addon3" value={url} onChange={e => setUrl(e.target.value)} />
+                        <button className="inline-block px-6 py-2 text-xs font-medium leading-tight uppercase bg-gray-900 border-2 border-gray-900 rounded btn text-gray-50 hover:bg-black focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3" onClick={getAlbum}>GO</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        {!goClicked
-            ?
-            <div className="flex justify-center ">
-                <button className="inline-block px-6 py-2 text-xs font-medium leading-tight uppercase bg-gray-900 border-2 border-gray-900 rounded btn text-gray-50 hover:bg-black focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
-                    <Link to={`/create`}>Create Playlist</Link>
-                </button>
-            </div>
-            :
-            <></>
-        }
-        {Object.keys(album).length !== 0 &&
-        <div>
-            <Album image={album.album_image} name={album.album_name} artist={album.artist} colors={album.image_colors} />
-            {album.new_request === true ? <Text text={"This is a new request"} loading={album.new_request}/> : <></>}
-        </div>
-        }
-        {renderNewAlbums
-            ?
-            <div class="grid grid-cols-4 grid-flow-row gap-4">
-                {
-                    recommendedAlbums.map((rec) =>
-                        <div>
-                            <Album image={rec.image} name={rec.name} artist={rec.artists} colors={rec.colors} />
+            {!goClicked
+                ?
+                <div className="flex justify-center ">
+                    <button className="inline-block px-6 py-2 text-xs font-medium leading-tight uppercase bg-gray-900 border-2 border-gray-900 rounded btn text-gray-50 hover:bg-black focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                        <Link to={`/create`}>Create Playlist</Link>
+                    </button>
+                </div>
+                :
+                <></>
+            }
+            {Object.keys(album).length !== 0 &&
+            <>
+                <Album image={album.album_image} name={album.album_name} artist={album.artist} colors={album.image_colors} />
+                <div class="mt-4 mb-4">
+                    <div class="relative rounded-xl">
+                        <div class="flex justify-center items-center">
+                            <button type="button" class="inline-flex items-center px-4 py-2 font-medium leading-tight text-xs shadow rounded text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed" disabled>
+                              <svg class="animate-spin h-5 w-5 mr-3 text-white" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                              </svg>
+                              PROCESSING...
+                            </button>
                         </div>
-                    )
-                }
-            </div>
-            :
-            <></>
-        }
+                    </div>
+                </div>
+            </>
+            }
+            {renderNewAlbums &&
+                <div class="grid grid-cols-4 grid-flow-row gap-4">
+                    {
+                        recommendedAlbums.map((rec) =>
+                            <div>
+                                <Album image={rec.image} name={rec.name} artist={rec.artists} colors={rec.colors} />
+                            </div>
+                        )
+                    }
+                </div>
+            }
         </>
     )
 }
